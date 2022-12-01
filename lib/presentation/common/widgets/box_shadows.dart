fb9535a426cb840705ea3a43f22b7251ce28b461
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-02 01:00:45 +0200

    create customBoxShadows

---
+++ lib/presentation/common/widgets/box_shadows.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:imdb/core/resources/color_manager.dart';
+
+List<BoxShadow> customBoxShadows() => [
+      BoxShadow(
+        color: ColorManager.grey.withOpacity(.2),
+        spreadRadius: 0.7,
+        blurRadius: 3,
+        offset: const Offset(1, 3),
+      ),
+      BoxShadow(
+        color: ColorManager.grey.withOpacity(.15),
+        spreadRadius: 0.5,
+        blurRadius: 1,
+        offset: const Offset(-1, 0),
+      ),
+    ];
