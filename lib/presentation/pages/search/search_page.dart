147ebac8a398c282ab5d238093e3167312a05db9
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-04 18:53:11 +0200

    create SearchPage

---
+++ lib/presentation/pages/search/search_page.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:imdb/config/routes/route_app.dart';
+import 'package:imdb/core/resources/color_manager.dart';
+import 'package:imdb/core/resources/styles_manager.dart';
+import 'package:imdb/core/utility/constant.dart';
+import 'package:imdb/presentation/common/widgets/box_shadows.dart';
+import 'package:imdb/presentation/pages/home/widgets/film_card.dart';
+import 'package:imdb/presentation/pages/video/video_page.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorManager.veryLowOpacityGrey2,
+      appBar: appBar(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: const [
+                    _RecentTexts(),
+                    _RecentlyVisited(),
+                    _MoviesText(),
+                  ],
+                ),
+              ),
+              Wrap(
+                crossAxisAlignment: WrapCrossAlignment.start,
+                spacing: 25,
+                runSpacing: 20,
+                children: [
+                  ...List.generate(6, (index) => const _ThumbnailTrailerCard()),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      actions: [
+        Flexible(
+          child: SizedBox(
+            width: double.infinity,
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: REdgeInsets.symmetric(horizontal: 15),
+                    child: const Icon(Icons.search),
+                  ),
+                  Text(
+                    "Search IMDb",
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                    style: getMediumStyle(fontSize: 20),
+                  ),
+                ]),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _MoviesText extends StatelessWidget {
+  const _MoviesText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: REdgeInsets.only(top: 30, bottom: horizontalPadding),
+      child: Row(
+        children: [
+          const Icon(Icons.movie_creation,
+              color: ColorManager.blackYellow, size: 33),
+          const RSizedBox(width: 20),
+          Expanded(child: Text("Movies", style: getBoldStyle(fontSize: 22))),
+        ],
+      ),
+    );
+  }
+}
+
+class _RecentTexts extends StatelessWidget {
+  const _RecentTexts({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: REdgeInsets.only(top: horizontalPadding, bottom: 10),
+      child: Row(
+        children: [
+          const Icon(Icons.access_time_outlined,
+              color: ColorManager.blackYellow, size: 33),
+          const RSizedBox(width: 20),
+          Expanded(child: Text("Recent", style: getBoldStyle(fontSize: 22))),
+          Text("CLEAR",
+              style: getMediumStyle(fontSize: 15, color: ColorManager.darkBlue))
+        ],
+      ),
+    );
+  }
+}
+
+class _RecentlyVisited extends StatelessWidget {
+  const _RecentlyVisited({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 180.h,
+      child: Scrollbar(
+        child: ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) => Padding(
+                  padding:
+                      REdgeInsetsDirectional.only(start: index == 0 ? 5 : 0),
+                  child: const Center(child: _ThumbnailTrailerCard()),
+                ),
+            separatorBuilder: (context, index) => const RSizedBox(width: 25),
+            itemCount: 5),
+      ),
+    );
+  }
+}
+
+class _ThumbnailTrailerCard extends StatelessWidget {
+  const _ThumbnailTrailerCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Go(context).offAll(const VideoPage());
+      },
+      child: Container(
+        height: 165.h,
+        width: 160.w,
+        padding: REdgeInsets.all(8),
+        decoration: BoxDecoration(
+            color: ColorManager.white,
+            borderRadius: BorderRadius.circular(7.r),
+            boxShadow: customBoxShadows()),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 100.h,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(7.r),
+                  child: const StaticImage()),
+            ),
+            SizedBox(
+              height: 48.h,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Top box office",
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: getNormalStyle(fontSize: 15),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
