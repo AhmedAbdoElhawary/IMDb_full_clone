7351547d167554b0d1249aecc5d721e13c28dbf8
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-04 21:36:12 +0200

    change the place of SearchResultPage

--- lib/presentation/common/widgets/text_field/mobile_phone_text_field.dart
+++ lib/presentation/common/widgets/text_field/mobile_phone_text_field.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:imdb/core/resources/color_manager.dart';
 import 'package:imdb/core/resources/styles_manager.dart';
-import 'package:imdb/presentation/common/widgets/text_field/custom_text_field.dart';
+import 'package:imdb/presentation/pages/search/widgets/search_text_field.dart';
 
 class MobilePhoneTextField extends StatefulWidget {
   final TextEditingController controller;
