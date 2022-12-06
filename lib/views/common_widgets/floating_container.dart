d624eb6df017516558a8b493a89929bdee35beae
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-06 13:02:04 +0200

    replace the file place

---
+++ lib/views/common_widgets/floating_container.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:imdb/helper/resources/color_manager.dart';
+
+class FloatingContainer extends StatelessWidget {
+  final double? height;
+  final Widget child;
+  final bool withPadding;
+  const FloatingContainer(
+      {super.key, this.height, this.withPadding = false, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Padding(
+      padding: REdgeInsets.only(top: withPadding ? 20 : 0),
+      child: Container(
+        height: height ?? screenHeight / 1.45,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: ColorManager.white,
+          boxShadow: [_cardBoxShadow()],
+        ),
+        child: child,
+      ),
+    );
+  }
+}
+
+BoxShadow _cardBoxShadow() {
+  return BoxShadow(
+    color: ColorManager.grey.withOpacity(.55),
+    spreadRadius: 0.5,
+    blurRadius: 3,
+    offset: const Offset(0, 3),
+  );
+}
