16fe527ec6e4dc6f449ec532105e49b32d692feb
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-06 13:01:46 +0200

    replace the file place

---
+++ lib/views/common_widgets/custom_outlined_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:imdb/helper/resources/color_manager.dart';
+
+class CustomOutlinedButton extends StatelessWidget {
+  final Widget child;
+  final VoidCallback onPressed;
+  final Color roundedColor;
+  const CustomOutlinedButton(
+      {Key? key,
+      required this.child,
+      required this.onPressed,
+      this.roundedColor = ColorManager.black54})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      style: outlinedStyle(),
+      onPressed: onPressed,
+      child: child,
+    );
+  }
+
+  ButtonStyle outlinedStyle() {
+    return OutlinedButton.styleFrom(
+      minimumSize: Size(double.infinity, 40.h),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3.0)),
+      side: BorderSide(width: 1, color: roundedColor),
+    );
+  }
+}
