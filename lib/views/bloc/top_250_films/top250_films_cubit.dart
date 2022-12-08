abca72bbfcabe566231ec1ab073abb546c40b4c0
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-08 18:15:17 +0200

    create Top250FilmsCubit

---
+++ lib/views/bloc/top_250_films/top250_films_cubit.dart
@@ -0,0 +1,38 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:imdb/helper/functions/api_result.dart';
+import 'package:imdb/helper/functions/network_exceptions.dart';
+import 'package:imdb/repositories/top_250_films_repo.dart';
+import 'package:imdb/view_models/film/film_details.dart';
+import 'package:imdb/views/bloc/cubit_result_state.dart';
+
+class Top250FilmsCubit extends Cubit<ResultState<FilmDetails>> {
+  final Top250FilmsRepo _top250filmsRepo;
+  Top250FilmsCubit(this._top250filmsRepo) : super(const initial());
+    static Top250FilmsCubit get(BuildContext context) =>
+      BlocProvider.of(context);
+  void getTop250Movies() async {
+    emit(const ResultState.loading());
+    ApiResult<FilmDetails> result =
+        await _top250filmsRepo.getTop250Movies();
+
+    result.when(success: (FilmDetails userData) {
+      emit(ResultState.success(userData));
+    }, failure: (NetworkExceptions networkExceptions) {
+      emit(ResultState.error(networkExceptions));
+    });
+  }
+
+    void getTop250TVs() async {
+    emit(const ResultState.loading());
+    ApiResult<FilmDetails> result =
+        await _top250filmsRepo.getTop250TVs();
+
+    result.when(success: (FilmDetails userData) {
+      emit(ResultState.success(userData));
+    }, failure: (NetworkExceptions networkExceptions) {
+      emit(ResultState.error(networkExceptions));
+    });
+  }
+}
