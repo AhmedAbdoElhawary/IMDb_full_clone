03030cfb07e85d89656e5b7876a56a51ba437c49
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-09 04:00:39 +0200

    change the name of some files

--- lib/views/bloc/top_250_films/top250_films_cubit.dart
+++ lib/views/bloc/top_250_films/top250_films_cubit.dart
@@ -3,32 +3,29 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:imdb/helper/functions/api_result.dart';
 import 'package:imdb/helper/functions/network_exceptions.dart';
 import 'package:imdb/repositories/top_250_films_repo.dart';
-import 'package:imdb/view_models/film/film_details.dart';
+import 'package:imdb/view_models/film/film_sub_details/film_sub_details.dart';
 import 'package:imdb/views/bloc/cubit_result_state.dart';
 
-class Top250FilmsCubit extends Cubit<ResultState<FilmDetails>> {
+class Top250FilmsCubit extends Cubit<ResultState<FilmSubDetails>> {
   final Top250FilmsRepo _top250filmsRepo;
   Top250FilmsCubit(this._top250filmsRepo) : super(const initial());
-    static Top250FilmsCubit get(BuildContext context) =>
-      BlocProvider.of(context);
+  static Top250FilmsCubit get(BuildContext context) => BlocProvider.of(context);
   void getTop250Movies() async {
     emit(const ResultState.loading());
-    ApiResult<FilmDetails> result =
-        await _top250filmsRepo.getTop250Movies();
+    ApiResult<FilmSubDetails> result = await _top250filmsRepo.getTop250Movies();
 
-    result.when(success: (FilmDetails userData) {
+    result.when(success: (FilmSubDetails userData) {
       emit(ResultState.success(userData));
     }, failure: (NetworkExceptions networkExceptions) {
       emit(ResultState.error(networkExceptions));
     });
   }
 
-    void getTop250TVs() async {
+  void getTop250TVs() async {
     emit(const ResultState.loading());
-    ApiResult<FilmDetails> result =
-        await _top250filmsRepo.getTop250TVs();
+    ApiResult<FilmSubDetails> result = await _top250filmsRepo.getTop250TVs();
 
-    result.when(success: (FilmDetails userData) {
+    result.when(success: (FilmSubDetails userData) {
       emit(ResultState.success(userData));
     }, failure: (NetworkExceptions networkExceptions) {
       emit(ResultState.error(networkExceptions));
