4582aaee7d916ad398cda842d7f12d0034e56e45
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-09 04:00:09 +0200

    change the name of some files

--- lib/views/bloc/most_popular_films/most_popular_films_cubit.dart
+++ lib/views/bloc/most_popular_films/most_popular_films_cubit.dart
@@ -2,31 +2,31 @@ import 'package:bloc/bloc.dart';
 import 'package:imdb/helper/functions/api_result.dart';
 import 'package:imdb/helper/functions/network_exceptions.dart';
 import 'package:imdb/repositories/most_popular_films_repo.dart';
-import 'package:imdb/view_models/film/film_details.dart';
+import 'package:imdb/view_models/film/film_sub_details/film_sub_details.dart';
 import 'package:imdb/views/bloc/cubit_result_state.dart';
 
-class MostPopularFilmsCubit extends Cubit<ResultState<FilmDetails>> {
+class MostPopularFilmsCubit extends Cubit<ResultState<FilmSubDetails>> {
   final MostPopularFilmsRepo _mostPopularFilmsRepo;
   MostPopularFilmsCubit(this._mostPopularFilmsRepo) : super(const initial());
 
   void getMostPopularMovies() async {
     emit(const ResultState.loading());
-    ApiResult<FilmDetails> result =
+    ApiResult<FilmSubDetails> result =
         await _mostPopularFilmsRepo.getMostPopularMovies();
 
-    result.when(success: (FilmDetails userData) {
+    result.when(success: (FilmSubDetails userData) {
       emit(ResultState.success(userData));
     }, failure: (NetworkExceptions networkExceptions) {
       emit(ResultState.error(networkExceptions));
     });
   }
 
-    void getMostPopularTVs() async {
+  void getMostPopularTVs() async {
     emit(const ResultState.loading());
-    ApiResult<FilmDetails> result =
+    ApiResult<FilmSubDetails> result =
         await _mostPopularFilmsRepo.getMostPopularTVs();
 
-    result.when(success: (FilmDetails userData) {
+    result.when(success: (FilmSubDetails userData) {
       emit(ResultState.success(userData));
     }, failure: (NetworkExceptions networkExceptions) {
       emit(ResultState.error(networkExceptions));
