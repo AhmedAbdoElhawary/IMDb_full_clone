e965823968c512da868c6281aa80e29465267938
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-09 03:59:38 +0200

    change the name of some files

--- lib/views/bloc/box_office/box_office_cubit.dart
+++ lib/views/bloc/box_office/box_office_cubit.dart
@@ -1,6 +1,39 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:imdb/helper/functions/api_result.dart';
+import 'package:imdb/helper/functions/network_exceptions.dart';
+import 'package:imdb/repositories/box_office_repo.dart';
+import 'package:imdb/view_models/box_office/box_office_details.dart';
+import 'package:imdb/view_models/box_office_of_all_time/box_office_of_all_time_details.dart';
 import 'package:imdb/views/bloc/cubit_result_state.dart';
 
 class BoxOfficeCubit extends Cubit<ResultState<dynamic>> {
-  BoxOfficeCubit() : super(const initial());
+ final BoxOfficeRepo _boxOfficeRepo;
+  BoxOfficeCubit(this._boxOfficeRepo) : super(const initial());
+    static BoxOfficeCubit get(BuildContext context) =>
+      BlocProvider.of(context);
+  void getBoxOffice() async {
+    emit(const ResultState.loading());
+    ApiResult<BoxOfficeDetails> result =
+        await _boxOfficeRepo.getBoxOffice();
+
+    result.when(success: (BoxOfficeDetails data) {
+      emit(ResultState.success(data));
+    }, failure: (NetworkExceptions networkExceptions) {
+      emit(ResultState.error(networkExceptions));
+    });
+  }
+
+    void getBoxOfficeAllTime() async {
+    emit(const ResultState.loading());
+    ApiResult<BoxOfficeOfAllTimeDetails> result =
+        await _boxOfficeRepo.getBoxOfficeAllTime();
+
+    result.when(success: (BoxOfficeOfAllTimeDetails data) {
+      emit(ResultState.success(data));
+    }, failure: (NetworkExceptions networkExceptions) {
+      emit(ResultState.error(networkExceptions));
+    });
+  }
 }
