1ac0b24af9824a76604a778316f5bffd9716ec8c
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-09 04:04:40 +0200

    create PersonalInfoApi

---
+++ lib/models/remote/personal_info/personal_info_api.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:imdb/helper/utility/constant.dart';
+import 'package:imdb/helper/utility/private_keys.dart';
+import 'package:imdb/view_models/personal_award/personal_award_details.dart';
+import 'package:imdb/view_models/personal_info/personal_info_details.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'personal_info_api.g.dart';
+
+@RestApi(baseUrl: baseUrl)
+abstract class PersonalInfoApi {
+  factory PersonalInfoApi(Dio dio, {String baseUrl}) = _PersonalInfoApi;
+
+  @GET("Name/{apiKey}/{id}")
+  Future<PersonalInfoDetails> getPersonalInfo(@Path() String id,
+      {@Path() final String apiKey = apiKey});
+  @GET("NameAwards/{apiKey}/{id}")
+  Future<PersonalAwardDetails> getPersonalAwards(@Path() String id,
+      {@Path() final String apiKey = apiKey});
+}
