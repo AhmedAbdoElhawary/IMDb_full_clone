9a4c0cff33fbabe05157b0f708aac308c5c8f984
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-09 03:59:40 +0200

    change the name of some files

--- lib/repositories/box_office_repo.dart
+++ lib/repositories/box_office_repo.dart
@@ -1,6 +1,6 @@
 import 'package:imdb/helper/functions/api_result.dart';
 import 'package:imdb/helper/functions/network_exceptions.dart';
-import 'package:imdb/models/box_office.dart';
+import 'package:imdb/models/remote/box_office/box_office.dart';
 import 'package:imdb/view_models/box_office/box_office_details.dart';
 import 'package:imdb/view_models/box_office_of_all_time/box_office_of_all_time_details.dart';
 
@@ -8,19 +8,18 @@ class BoxOfficeRepo {
   final BoxOfficeApi _boxOfficeApi;
   BoxOfficeRepo(this._boxOfficeApi);
 
-  Future<ApiResult<List<BoxOfficeDetails>>> getBoxOffice() async {
+  Future<ApiResult<BoxOfficeDetails>> getBoxOffice() async {
     try {
-      List<BoxOfficeDetails> data = await _boxOfficeApi.getBoxOffice();
+      BoxOfficeDetails data = await _boxOfficeApi.getBoxOffice();
       return ApiResult.success(data);
     } catch (e) {
       return ApiResult.failure(NetworkExceptions.getDioException(e));
     }
   }
 
-  Future<ApiResult<List<BoxOfficeOfAllTimeDetails>>>
-      getBoxOfficeAllTime() async {
+  Future<ApiResult<BoxOfficeOfAllTimeDetails>> getBoxOfficeAllTime() async {
     try {
-      List<BoxOfficeOfAllTimeDetails> data =
+      BoxOfficeOfAllTimeDetails data =
           await _boxOfficeApi.getBoxOfficeAllTime();
       return ApiResult.success(data);
     } catch (e) {
