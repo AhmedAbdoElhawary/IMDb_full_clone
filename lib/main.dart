745c229fc1b2779f93e55054e88a6a459cb4607c
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-09 04:00:59 +0200

    add some comments in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:imdb/helper/utility/injector.dart';
 import 'package:imdb/views/my_app.dart';
 
+/// Note
+/// **************************************************************************
+/// Film: refer to movie and TV series together
+/// When i want to specify, i will say movie or series
+/// **************************************************************************
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
