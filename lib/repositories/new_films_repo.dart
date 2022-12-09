955f07ff5ee4a66fcceaf374d40b374473c9ce76
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-09 04:00:35 +0200

    change the name of some files

--- lib/repositories/new_films_repo.dart
+++ lib/repositories/new_films_repo.dart
@@ -1,24 +1,24 @@
 import 'package:imdb/helper/functions/api_result.dart';
 import 'package:imdb/helper/functions/network_exceptions.dart';
-import 'package:imdb/models/new_films_api.dart';
+import 'package:imdb/models/remote/new_films/new_films_api.dart';
 import 'package:imdb/view_models/in_theaters/new_film_details.dart';
 
 class NewFilmsRepo {
   final NewFilmsApi _newFilmsApi;
   NewFilmsRepo(this._newFilmsApi);
 
-  Future<ApiResult<List<NewFilmDetails>>> getComingSoon() async {
+  Future<ApiResult<NewFilmDetails>> getComingSoon() async {
     try {
-      List<NewFilmDetails> data = await _newFilmsApi.getComingSoon();
+      NewFilmDetails data = await _newFilmsApi.getComingSoon();
       return ApiResult.success(data);
     } catch (e) {
       return ApiResult.failure(NetworkExceptions.getDioException(e));
     }
   }
 
-  Future<ApiResult<List<NewFilmDetails>>> getInTheaters() async {
+  Future<ApiResult<NewFilmDetails>> getInTheaters() async {
     try {
-      List<NewFilmDetails> data = await _newFilmsApi.getInTheaters();
+      NewFilmDetails data = await _newFilmsApi.getInTheaters();
       return ApiResult.success(data);
     } catch (e) {
       return ApiResult.failure(NetworkExceptions.getDioException(e));
