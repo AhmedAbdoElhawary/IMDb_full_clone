adcc6e9b316dbf42ec517e5d8e80040b8ca1ea65
ahmed <ahmedabdoelhawari12345work@gmail.com>
2022-12-10 06:38:36 +0200

    continue building the ui

---
+++ lib/views/bloc/search/search_bloc.dart
@@ -0,0 +1,14 @@
+// import 'package:bloc/bloc.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
+// import 'package:imdb/views/bloc/cubit_result_state.dart';
+
+// part 'search_event.dart';
+// part 'search_bloc.freezed.dart';
+
+// class SearchBloc extends Bloc<SearchEvent, ResultState<dynamic>> {
+//   SearchBloc() : super(Initial()) {
+//     on<SearchEvent>((event, emit) {
+//       // TODO: implement event handler
+//     });
+//   }
+// }
