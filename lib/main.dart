00c2287d912cb0a0fa37982e83dfbea4f85f4ae7
“ahmed <ahmedabdoelhawari12345work@gmail.com>
2023-03-05 16:43:41 +0200

    solve some issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:imdb/helper/utility/injector.dart';
-import 'package:imdb/helper/utility/private_key.dart';
 import 'package:imdb/views/my_app.dart';
 
 /// Note
@@ -12,16 +10,6 @@ import 'package:imdb/views/my_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await initializeDependencies();
-  // final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
-  // final googleAuth = await googleUser?.authentication;
-  // String? token = googleAuth?.accessToken ?? "";
-  // accessToken = token;
-  // print("================> $accessToken");
   runApp(const MyApp());
 }
-//
-// final GoogleSignIn googleSignIn =
-// GoogleSignIn(scopes: ['https://www.googleapis.com/auth/youtube.force-ssl']);
-//
--- lib/views/common_widgets/floating_container.dart
+++ lib/views/common_widgets/floating_container.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:imdb/helper/resources/color_manager.dart';
 
 class FloatingContainer extends StatelessWidget {
